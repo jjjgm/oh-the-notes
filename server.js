b11936fed7896c98ee404c1fa4fa658ddd042ae1
jjjgm <businessjgm@icloud.com>
2023-04-08 02:17:43 -0700

    adding some extra files and server code

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
+const PORT = process.env. PORT || 3001;
 
-// const PORT = ;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
