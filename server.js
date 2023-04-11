ee2ef734f8b13ce0ca58c1eb80d282bc696b23d7
jjjgm <businessjgm@icloud.com>
2023-04-10 22:58:29 -0700

    some restructuring

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const app = express();
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env. PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express();
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// Data will be stored in the db.json file
+const dbPath = path.join(__dirname, './db/db.json')
+
+
+
+// To write new data into db.json in string format else creates an error
+const writeToFile = (data, callback) => {
+    fs.writeFile(dbPath, JSON.stringify(parsedJson), err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('note writing');
+    });
+}
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
